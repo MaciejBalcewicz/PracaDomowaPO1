a8033aa40f3d73f643d850e655d92d4265f1f8c6
MaciejBalcewicz <maciej.balcewicz@onet.pl>
2023-10-18 11:03:22 +0200

    first commit

---
+++ zad 2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj temperature w stopniach Celsjusza");
+        double Celsjusz = double.Parse(Console.ReadLine());
+        double F = 32.0 + (9.0 / 5.0) * Celsjusz;
+        Console.WriteLine(F);
+        Console.ReadKey();
+    }
+}
