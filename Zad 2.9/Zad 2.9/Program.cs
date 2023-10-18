962aedec45e12e85bf3da53b6c658ae83e5fd2c0
MaciejBalcewicz <122689340+MaciejBalcewicz@users.noreply.github.com>
2023-10-18 11:14:56 +0200

    Add files via upload

---
+++ Zad 2.9/Zad 2.9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Zad_2._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a
+            int x = 1, y = 4, z = 2;
+            bool wynikA = (x++ > 1 && y++ == 4 && z-- > 0);
+            Console.WriteLine("wynikA={0} x={1} y={2} z={3}", wynikA, x, y, z);
+
+            // b
+            x = 1; y = 4; z = 2; 
+            bool wynikB = (x++ > 1 & y++ == 4 && z-- > 0);
+            Console.WriteLine("wynikB={0} x={1} y={2} z={3}", wynikB, x, y, z);
+
+            // c
+            x = 1; y = 4; z = 2; 
+            bool wynikC = (x++ > 1 & y++ == 4 & z-- > 0);
+            Console.WriteLine("wynikC={0} x={1} y={2} z={3}", wynikC, x, y, z);
+
+            // d
+            x = 1; y = 3; z = 4; 
+            bool wynikD = (x == 1 || y++ > 2 || ++z > 0);
+            Console.WriteLine("wynikD={0} x={1} y={2} z={3}", wynikD, x, y, z);
+
+            // e
+            x = 1; y = 3; z = 4; 
+            bool wynikE = (x == 1 | y++ > 2 || ++z > 0);
+            Console.WriteLine("wynikE={0} x={1} y={2} z={3}", wynikE, x, y, z);
+
+            // f
+            x = 1; y = 3; z = 4; 
+            bool wynikF = (x == 1 | y++ > 2 | ++z > 0);
+            Console.WriteLine("wynikF={0} x={1} y={2} z={3}", wynikF, x, y, z);
+            Console.ReadLine();
+        }
+       
+    }
+}
---
+++ zad 2.8/zad 2.8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace zad_2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x;
+            int y = 1, z = 1;
+            x = (y == 1 && z++ == 1);
+
+            Console.WriteLine($"x: {x}, y: {y}, z: {z}");
+            Console.ReadLine();
+        }
+    }
+}
