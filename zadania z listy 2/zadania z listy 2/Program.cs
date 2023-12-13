23a92a674852192fbf8fa6bcf9f9fa832964a368
MaciejBalcewicz <122689340+MaciejBalcewicz@users.noreply.github.com>
2023-12-13 13:17:48 +0100

    Add files via upload

---
+++ zadania z listy 2/zadania z listy 2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int liczba3 = 0;
+            int liczba1 = 30 / liczba3;
+            Console.WriteLine("Nie zostanie wyświetlone, bo dzielimy przez 0");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine($"Typ wyjątku: {e.GetType()}");
+            Console.WriteLine($"StackTrace: {e.StackTrace}");
+        }
+        finally
+        {
+            Console.WriteLine("FINALLY");
+        }
+
+        try
+        {
+            int[] table = new int[2];
+            table[25] = 2;
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Console.WriteLine($"Wyjątek IndexOutOfRangeException: {e.Message}");
+        }
+        catch
+        {
+            Console.WriteLine("Inny wyjątek");
+        }
+
+        try
+        {
+            throw new IndexOutOfRangeException();
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Console.WriteLine($"Przechwycono wyjątek: {e.Message}");
+        }
+        Console.ReadLine();
+    }
+   
+}
+
+class Car
+{
+    private double pojemnoscSilnika;
+    private string marka;
+
+    private Car()
+    {
+        pojemnoscSilnika = 0.0;
+        marka = "";
+    }
+
+    public Car(double pojemnoscSilnika, string marka)
+    {
+        this.pojemnoscSilnika = pojemnoscSilnika;
+        this.marka = marka;
+    }
+
+    public static Car Create()
+    {
+        return new Car();
+    }
+
+    ~Car()
+    {
+        Console.WriteLine("Zwalniam pamiec");
+    }
+
+    public static int iloscKol;
+
+    static Car()
+    {
+        iloscKol = 4;
+    }
+}
+
---
+++ zadania z listy 3/zadania z listy 3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+class Obliczenia
+{
+    private double wynik;
+
+    public void Dodawanie(double a, double b)
+    {
+        wynik = a + b;
+    }
+
+    public void Odejmowanie(double a, double b)
+    {
+        wynik = a - b;
+    }
+
+    public void Mnozenie(double a, double b)
+    {
+        wynik = a * b;
+    }
+
+    public void Dzielenie(double a, double b)
+    {
+        if (b != 0)
+        {
+            wynik = a / b;
+        }
+        else
+        {
+            Console.WriteLine("Nie można dzielić przez zero.");
+        }
+    }
+
+    public void Potegowanie(double podstawa, double wykladnik)
+    {
+        wynik = Math.Pow(podstawa, wykladnik);
+    }
+
+    public void Pierwiastkowanie(double liczba, double stopien)
+    {
+        if (liczba >= 0)
+        {
+            wynik = Math.Pow(liczba, 1.0 / stopien);
+        }
+        else
+        {
+            Console.WriteLine("Nie można pierwiastkować liczby ujemnej.");
+        }
+    }
+
+    public double PobierzWynik()
+    {
+        return wynik;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Obliczenia kalkulator = new Obliczenia();
+
+            Console.Write("Podaj pierwszą liczbę: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Podaj drugą liczbę: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            kalkulator.Dodawanie(a, b);
+            Console.WriteLine($"Wynik dodawania: {kalkulator.PobierzWynik()}");
+
+            kalkulator.Odejmowanie(a, b);
+            Console.WriteLine($"Wynik odejmowania: {kalkulator.PobierzWynik()}");
+
+            kalkulator.Mnozenie(a, b);
+            Console.WriteLine($"Wynik mnożenia: {kalkulator.PobierzWynik()}");
+
+            kalkulator.Dzielenie(a, b);
+            Console.WriteLine($"Wynik dzielenia: {kalkulator.PobierzWynik()}");
+
+            kalkulator.Potegowanie(a, b);
+            Console.WriteLine($"Wynik potęgowania: {kalkulator.PobierzWynik()}");
+
+            kalkulator.Pierwiastkowanie(a, b);
+            Console.WriteLine($"Wynik pierwiastkowania: {kalkulator.PobierzWynik()}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Błąd formatu danych. Podaj poprawne liczby.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+    }
+}
