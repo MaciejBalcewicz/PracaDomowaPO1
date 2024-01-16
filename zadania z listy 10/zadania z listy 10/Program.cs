1bb8db657d0ffbde778043737f65af8457a165be
MaciejBalcewicz <122689340+MaciejBalcewicz@users.noreply.github.com>
2024-01-16 11:19:34 +0100

    Add files via upload

---
+++ zadania z listy 10/zadania z listy 10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
+
+class Program
+{
+    const int _max = 100000;
+
+    static void Main()
+    {
+        var list = new List<string>();
+        var link = new LinkedList<string>();
+        var queue = new Queue<string>();
+        var hashSet = new HashSet<string>();
+
+        // ... Add elements.
+        for (int i = 0; i < 1000; i++)
+        {
+            list.Add("OK");
+            link.AddLast("OK");
+            queue.Enqueue("OK");
+            hashSet.Add("OK");
+        }
+
+        TestCollection(list, "List");
+        TestCollection(link, "LinkedList");
+        TestCollection(queue, "Queue");
+        TestCollection(hashSet, "HashSet");
+
+        Console.Read();
+    }
+
+    static void TestCollection(IEnumerable collection, string collectionName)
+    {
+        var sw = Stopwatch.StartNew();
+        for (int i = 0; i < _max; i++)
+        {
+            foreach (string v in collection)
+            {
+                if (v.Length != 2)
+                {
+                    throw new Exception();
+                }
+            }
+        }
+        sw.Stop();
+        Console.WriteLine($"{collectionName}: {((double)(sw.Elapsed.TotalMilliseconds * 1000000) / _max).ToString("0.00 ns")}");
+    }
+}
